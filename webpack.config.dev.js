e4e3683c4ed4b6ab671d701b62b26c75c6d43162
Anton Fedorovskyi <a.fedorovskyi@plarium.com>
2021-02-11 17:20:43 +0200

    added prod config

---
+++ webpack.config.dev.js
@@ -0,0 +1,68 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+var babelOptions = {
+  presets: ["@babel/preset-env", "@babel/preset-react"],
+};
+
+module.exports = {
+  mode: "development",
+  devtool: "source-map",
+  target: "web",
+
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+
+  entry: {
+    index: path.join(__dirname, "src/index.tsx"),
+    vendor: ["babel-polyfill", "react", "react-dom"],
+  },
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+    chunkFilename: "[chunkhash].js",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+          },
+          {
+            loader: "ts-loader",
+          },
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+          },
+        ],
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+      inject: true,
+    }),
+  ],
+
+  devServer: {
+    contentBase: path.resolve(__dirname, "dist"),
+    publicPath: "/public/",
+    watchContentBase: true,
+    open: true,
+    hot: true,
+  },
+};
